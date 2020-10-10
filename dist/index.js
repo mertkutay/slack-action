6c270c43e1f5eaa495979f9a83efd71dc9c74e43
Mert Kutay <mertckutay@gmail.com>
2020-10-11 02:11:01 +0300

    compile

--- dist/index.js
+++ dist/index.js
@@ -7,49 +7,59 @@ module.exports =
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _slack_webhook__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2881);
-const core = __webpack_require__(691);
-const github = __webpack_require__(4737);
+/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4737);
+/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(691);
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _slack_webhook__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2881);
+
+
 
 
 async function run() {
   try {
-    const status = core.getInput("status", { required: true });
-    const channel = core.getInput("channel", { required: false });
-    const workflow = context.workflow;
-    const repository = context.payload.repository;
+    const webhookUrl = process.env.SLACK_WEBHOOK_URL;
+    const status = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('status', { required: true });
+    const channel = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('channel', { required: false });
+    const { workflow, payload } = _actions_github__WEBPACK_IMPORTED_MODULE_0__.context;
+    const { repository, sender, compare } = payload;
+    const commit = payload.head_commit;
+    const branch = _actions_github__WEBPACK_IMPORTED_MODULE_0__.context.ref.replace('refs/heads/', '');
     const repositoryName = repository.full_name;
     const repositoryUrl = repository.html_url;
-    const sender = context.payload.sender;
 
-    let text = "";
+    let text = '';
     let color;
     switch (status.toLowerCase()) {
-      case "started":
+      case 'started':
         text = `:information_source: Started: ${workflow}`;
-      case "success":
-        color = "good";
+        break;
+      case 'success':
+        color = 'good';
         text = `:white_check_mark: Success: ${workflow}`;
-      case "failure":
-        color = "danger";
+        break;
+      case 'failure':
+        color = 'danger';
         text = `:no_entry: Failure: ${workflow}`;
-      case "cancelled":
-        color = "warning";
+        break;
+      case 'cancelled':
+        color = 'warning';
         text = `:warning: Cancelled: ${workflow}`;
+        break;
+      default:
+        throw new Error(
+          'Valid statuses are: started, success, failure, cancelled',
+        );
     }
-
-    const commit = context.payload.head_commit;
-    const compare = context.payload.compare;
-    const branch = context.ref.replace("refs/heads/", "");
 
     const fields = [
       {
-        title: "Branch",
+        title: 'Branch',
         value: `<${compare}|${branch}>`,
         short: false,
       },
       {
-        title: "Commit",
+        title: 'Commit',
         value: `<${commit.url}|${commit.message}>`,
         short: false,
       },
@@ -63,19 +73,22 @@ async function run() {
       attachments: [
         {
           fallback: `New Commit on ${repositoryName}/${branch} by ${sender.login}`,
-          color: color,
+          color,
           author_name: sender.login,
           author_link: sender.html_url,
           author_icon: sender.avatar_url,
           fields,
           footer: `<${repositoryUrl}|${repositoryName}>`,
-          footer_icon: "https://github.githubassets.com/favicon.ico",
+          footer_icon: 'https://github.githubassets.com/favicon.ico',
           ts: ts.getTime().toString(),
         },
       ],
     };
+
+    const webhook = new _slack_webhook__WEBPACK_IMPORTED_MODULE_2__/* .IncomingWebhook */ .QU(webhookUrl);
+    await webhook.send(message);
   } catch (error) {
-    core.setFailed(error.message);
+    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed(error.message);
   }
 }
 
@@ -3701,7 +3714,7 @@ var __webpack_unused_export__;
 /// <reference lib="es2017" />
 __webpack_unused_export__ = ({ value: true });
 var IncomingWebhook_1 = __webpack_require__(4008);
-__webpack_unused_export__ = IncomingWebhook_1.IncomingWebhook;
+exports.QU = IncomingWebhook_1.IncomingWebhook;
 var errors_1 = __webpack_require__(9841);
 __webpack_unused_export__ = errors_1.ErrorCode;
 //# sourceMappingURL=index.js.map
@@ -6408,7 +6421,7 @@ exports.useColors = useColors;
 exports.colors = [ 6, 2, 3, 4, 5, 1 ];
 
 try {
-  var supportsColor = __webpack_require__(8265);
+  var supportsColor = __webpack_require__(6361);
   if (supportsColor && supportsColor.level >= 2) {
     exports.colors = [
       20, 21, 26, 27, 32, 33, 38, 39, 40, 41, 42, 43, 44, 45, 56, 57, 62, 63, 68,
@@ -6923,6 +6936,22 @@ function wrap(protocols) {
 // Exports
 module.exports = wrap({ http: http, https: https });
 module.exports.wrap = wrap;
+
+
+/***/ }),
+
+/***/ 5505:
+/***/ ((module) => {
+
+"use strict";
+
+module.exports = (flag, argv) => {
+	argv = argv || process.argv;
+	const prefix = flag.startsWith('-') ? '' : (flag.length === 1 ? '-' : '--');
+	const pos = argv.indexOf(prefix + flag);
+	const terminatorPos = argv.indexOf('--');
+	return pos !== -1 && (terminatorPos === -1 ? true : pos < terminatorPos);
+};
 
 
 /***/ }),
@@ -8838,6 +8867,145 @@ function onceStrict (fn) {
 
 /***/ }),
 
+/***/ 6361:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+
+const os = __webpack_require__(2087);
+const hasFlag = __webpack_require__(5505);
+
+const env = process.env;
+
+let forceColor;
+if (hasFlag('no-color') ||
+	hasFlag('no-colors') ||
+	hasFlag('color=false')) {
+	forceColor = false;
+} else if (hasFlag('color') ||
+	hasFlag('colors') ||
+	hasFlag('color=true') ||
+	hasFlag('color=always')) {
+	forceColor = true;
+}
+if ('FORCE_COLOR' in env) {
+	forceColor = env.FORCE_COLOR.length === 0 || parseInt(env.FORCE_COLOR, 10) !== 0;
+}
+
+function translateLevel(level) {
+	if (level === 0) {
+		return false;
+	}
+
+	return {
+		level,
+		hasBasic: true,
+		has256: level >= 2,
+		has16m: level >= 3
+	};
+}
+
+function supportsColor(stream) {
+	if (forceColor === false) {
+		return 0;
+	}
+
+	if (hasFlag('color=16m') ||
+		hasFlag('color=full') ||
+		hasFlag('color=truecolor')) {
+		return 3;
+	}
+
+	if (hasFlag('color=256')) {
+		return 2;
+	}
+
+	if (stream && !stream.isTTY && forceColor !== true) {
+		return 0;
+	}
+
+	const min = forceColor ? 1 : 0;
+
+	if (process.platform === 'win32') {
+		// Node.js 7.5.0 is the first version of Node.js to include a patch to
+		// libuv that enables 256 color output on Windows. Anything earlier and it
+		// won't work. However, here we target Node.js 8 at minimum as it is an LTS
+		// release, and Node.js 7 is not. Windows 10 build 10586 is the first Windows
+		// release that supports 256 colors. Windows 10 build 14931 is the first release
+		// that supports 16m/TrueColor.
+		const osRelease = os.release().split('.');
+		if (
+			Number(process.versions.node.split('.')[0]) >= 8 &&
+			Number(osRelease[0]) >= 10 &&
+			Number(osRelease[2]) >= 10586
+		) {
+			return Number(osRelease[2]) >= 14931 ? 3 : 2;
+		}
+
+		return 1;
+	}
+
+	if ('CI' in env) {
+		if (['TRAVIS', 'CIRCLECI', 'APPVEYOR', 'GITLAB_CI'].some(sign => sign in env) || env.CI_NAME === 'codeship') {
+			return 1;
+		}
+
+		return min;
+	}
+
+	if ('TEAMCITY_VERSION' in env) {
+		return /^(9\.(0*[1-9]\d*)\.|\d{2,}\.)/.test(env.TEAMCITY_VERSION) ? 1 : 0;
+	}
+
+	if (env.COLORTERM === 'truecolor') {
+		return 3;
+	}
+
+	if ('TERM_PROGRAM' in env) {
+		const version = parseInt((env.TERM_PROGRAM_VERSION || '').split('.')[0], 10);
+
+		switch (env.TERM_PROGRAM) {
+			case 'iTerm.app':
+				return version >= 3 ? 3 : 2;
+			case 'Apple_Terminal':
+				return 2;
+			// No default
+		}
+	}
+
+	if (/-256(color)?$/i.test(env.TERM)) {
+		return 2;
+	}
+
+	if (/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i.test(env.TERM)) {
+		return 1;
+	}
+
+	if ('COLORTERM' in env) {
+		return 1;
+	}
+
+	if (env.TERM === 'dumb') {
+		return min;
+	}
+
+	return min;
+}
+
+function getSupportLevel(stream) {
+	const level = supportsColor(stream);
+	return translateLevel(level);
+}
+
+module.exports = {
+	supportsColor: getSupportLevel,
+	stdout: getSupportLevel(process.stdout),
+	stderr: getSupportLevel(process.stderr)
+};
+
+
+/***/ }),
+
 /***/ 6833:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -9192,14 +9360,6 @@ module.exports = eval("require")("encoding");
 
 /***/ }),
 
-/***/ 8265:
-/***/ ((module) => {
-
-module.exports = eval("require")("supports-color");
-
-
-/***/ }),
-
 /***/ 7716:
 /***/ ((module) => {
 
@@ -9360,6 +9520,35 @@ module.exports = require("zlib");
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => module['default'] :
+/******/ 				() => module;
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
